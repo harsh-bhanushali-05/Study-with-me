28a4ef4b89979fe6f61bf31bdc3a7263dc492528
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2023-12-01 12:25:56 +0530

    Add files via upload
    Added a notification to the user about the change in the mode of timer -- > work to break and break to work.

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -4,19 +4,21 @@ import "../Pomodoro.css";
 import Timmer from "./Timmer";
 import Buttons from "./Buttons";
 import "react-circular-progressbar/dist/styles.css";
+
 function Pomodoro() {
- 
   const [time, setTime] = useState(1500); // setting timer for 25 mins
   const [mode, setMode] = useState("work");
-  const [workTime, SetWrokTime] = useState(1500);
-  const [BreakTime, SetBreakTime] = useState(300);
+  const [workTime, SetWorkTime] = useState(1500);
+  const [breakTime, SetBreakTime] = useState(300);
   const [isActive, setIsActive] = useState(false);
   const [percentage, setPercentage] = useState(100);
-  function calTime(){
-    let min = Math.floor(time/60);
-    let sec=Math.floor(time%60);
-    return `${min}m ${sec}s`;    
+
+  function calTime() {
+    let min = Math.floor(time / 60);
+    let sec = Math.floor(time % 60);
+    return `${min}m ${sec}s`;
   }
+
   useEffect(() => {
     let interval;
     if (isActive) {
@@ -24,11 +26,13 @@ function Pomodoro() {
         if (time === 0) {
           clearInterval(interval);
           if (mode === "work") {
-            setTime(BreakTime);
+            setTime(breakTime);
             setMode("Break");
+            showNotification("Break Time", "Take a break!");
           } else {
             setTime(workTime);
             setMode("work");
+            showNotification("Work Time", "Back to work!");
           }
           setPercentage(100);
         } else {
@@ -36,7 +40,7 @@ function Pomodoro() {
           if (mode === "work") {
             setPercentage(Math.floor((time / workTime) * 100));
           } else {
-            setPercentage(Math.floor((time / BreakTime) * 100));
+            setPercentage(Math.floor((time / breakTime) * 100));
           }
         }
       }, 1000);
@@ -44,39 +48,48 @@ function Pomodoro() {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, time]);
-  useEffect(()=>{
+  }, [isActive, time, mode, workTime, breakTime]);
+
+  useEffect(() => {
     setPercentage(100);
-    setMode('work');
+    setMode("work");
     setTime(workTime);
     setIsActive(false);
+  }, [workTime, breakTime]);
 
-  },[workTime,BreakTime])
+  const showNotification = (title, body) => {
+    if (Notification.permission === "granted") {
+      new Notification(title, { body });
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, { body });
+        }
+      });
+    }
+  };
+
   return (
     <div className="Time-col">
       <div className="progressbar-container">
-      <CircularProgressbar
+        <CircularProgressbar
           className="ProgressBar"
           value={percentage}
           text={`${calTime()}`}
           styles={{
-            // Customize the root progress bar element
-            // Customize the path, trail, and trail background
-            path: { stroke: '#FFD043' },
-            trail: { stroke: '#FFD095' },
-            // Customize the text
-            text: { fill: '#312F2F', fontSize: '20px' },
+            path: { stroke: "#FFD043" },
+            trail: { stroke: "#FFD095" },
+            text: { fill: "#312F2F", fontSize: "20px" },
           }}
           textStyles={{
-            // Customize the styles for the text value
-            value: { fontFamily: 'Arial', fontWeight: 'bold' },
+            value: { fontFamily: "Arial", fontWeight: "bold" },
           }}
         />
-    </div>
-      <Buttons isCounting={setIsActive}/>
+      </div>
+      <Buttons isCounting={setIsActive} />
       <div className="time">
-      <Timmer break={SetWrokTime} def={25} min={5} max={60}/>
-      <Timmer break={SetBreakTime} def={5} min={1} max={30}/>
+        <Timmer break={SetWorkTime} def={25} min={5} max={60} />
+        <Timmer break={SetBreakTime} def={5} min={1} max={30} />
       </div>
     </div>
   );
